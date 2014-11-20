063c6dde3a1fabb9c155245d092cb96346d80198
Harpreet Singh <singhh17@uw.edu>
2014-11-19 23:57:57 -0800

    completed challenge

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ajax-challenge
 ==============
 
 File stubs and assets for the AJAX/Parse.com challenge
+
+This is a product review form and comments list page for the ajax challenge in info 343. 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,100 @@
 "use strict";
-/*
-    app.js, main Angular application script
-    define your module and controllers here
-*/
+
+
+var reviewsUrl = 'https://api.parse.com/1/classes/reviews';
+
+angular.module('ProductReview', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = '4cPKj6vDN36VPI8aCZg6PQ0cevcxOT3XvG4FHYsG';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'fmgOcJAYCVJO8UUgw05580IIM7vvwCRTdQe8SMIz';
+    })
+
+    .controller('ReviewsController', function($scope, $http) {
+        //refreshes the list of comments by getting feed from parse.com
+        $scope.refreshReviews = function() {
+            $http.get(reviewsUrl + '?order=-votes')
+                .success(function(data) {
+                    $scope.reviews = data.results;
+                });
+        };
+
+        $scope.refreshReviews();
+
+        //each new review is given the delete property of false
+        //so that it won't be deleted.
+        $scope.newReview = {remove: false};
+
+        //the variable for checking if a review was added successfully
+        $scope.successfulFeed = false;
+
+        //adds the review data to parse.com
+        $scope.addReview = function() {
+            $scope.inserting = true;
+            $http.post(reviewsUrl, $scope.newReview)
+                .success(function(responseData) {
+                    $scope.newReview.objectId = responseData.objectId;
+                    $scope.reviews.push($scope.newReview);
+                    $scope.newReview = {remove: false};
+                    $scope.successfulFeed = true;
+                })
+                .finally(function() {
+                    $scope.inserting = false;
+                });
+        };
+
+        //updates the review data on parse.com
+        $scope.updateReview = function(review) {
+            $scope.updating = true;
+            $http.put(reviewsUrl + '/' + review.objectId, review)
+                .success(function() {
+                    console.log("success")
+                })
+
+                .error(function(err) {
+                    console.log(err);
+                })
+
+                .finally(function() {
+                    $scope.updating = false;
+                });
+        };
+
+        //increments/decrements the vote count for the specified review
+        $scope.incrementVotes = function(review, amount) {
+            $scope.updating = true;
+            var postData = {
+                votes: {
+                    __op: "Increment",
+                    amount: amount
+                }
+            };
+
+            $http.put(reviewsUrl + '/' + review.objectId, postData)
+                .success(function(respData) {
+                    review.votes = respData.votes;
+                })
+                
+                .error(function(err) {
+                    console.log(err);
+                })
+
+                .finally(function() {
+                    $scope.updating = false;
+                });
+        };
+
+        //deletes the specified review from parse.com and the webpage.
+        $scope.removalUpdate = function(review) {
+            if (review.remove == true) {
+                $http.delete(reviewsUrl + '/' + review.objectId)
+                    .success(function() {
+                        $scope.refreshReviews()
+                    })
+                    .error(function(err) {
+                        console.log(err);
+                    });  
+            }
+        }
+    });
+
